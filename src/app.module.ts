775ba3efeb209efa3107a0af8b3aca834562a002
Mateus <mateusc.rossetto@hotmail.com>
2022-07-04 21:29:34 -0300

    create database mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://timer_nest:timer_nest@cluster0.msdpbin.mongodb.net/test',
+    ),
+  ],
   controllers: [],
   providers: [],
 })
