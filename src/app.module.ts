b9b2c3a5b1310195be519430ba294662537c136d
Mateus <mateusc.rossetto@hotmail.com>
2022-07-23 01:15:20 -0300

    feat: add .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     UsersModule,
-    MongooseModule.forRoot(
-      'mongodb+srv://timer_nest:timer_nest@cluster0.msdpbin.mongodb.net/test',
-    ),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
   ],
   controllers: [],
   providers: [],
